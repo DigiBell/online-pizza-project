d2763751f2c4e189be7b97a0e40cd7db52f9edbb
Anna_Ahlquist <DigiBell@users.noreply.github.com>
2020-06-29 22:59:31 +0200

    AddPizza gui and logic complete
    Tested, working. Maybe add more checks.

--- src/main/java/com/mycompany/onlinepizzaproject/ManagerAddPizzaController.java
+++ src/main/java/com/mycompany/onlinepizzaproject/ManagerAddPizzaController.java
@@ -1,10 +1,13 @@
 package com.mycompany.onlinepizzaproject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.mycompany.onlinepizzaproject.backend.API;
 import com.mycompany.onlinepizzaproject.backend.Ingredient;
+import com.mycompany.onlinepizzaproject.backend.Measurement;
+import com.mycompany.onlinepizzaproject.backend.Measurement.Unit;
 import com.mycompany.onlinepizzaproject.backend.Pizza;
 
 import javafx.collections.FXCollections;
@@ -17,7 +20,6 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import jdk.nashorn.api.tree.ForInLoopTree;
 
 public class ManagerAddPizzaController {
 	@FXML private MainController mainController;
@@ -29,6 +31,9 @@ public class ManagerAddPizzaController {
 	@FXML private ChoiceBox<String> add_pizza_ingredient_amount_units_choicebox = new ChoiceBox<String>();
 	@FXML private TextField add_pizza_ingredient_amount_textfield;
 	@FXML private TextArea add_pizza_ingredients_textarea;
+	private List<String> ingredientsText = new ArrayList<>();
+	private String strIngredient = "";
+	private HashMap<String, Measurement> ingredients = new HashMap<>();
 	
 	
 	private Stage stage;
@@ -52,11 +57,46 @@ public class ManagerAddPizzaController {
 	 
 	 @FXML
 	 private void addIngredient(ActionEvent event) {	
+
+		 int indexIngredient = add_pizza_ingredients_choicebox.getSelectionModel().getSelectedIndex();
+		 int indexIUnit = add_pizza_ingredient_amount_units_choicebox.getSelectionModel().getSelectedIndex();
+		 String ingredient = add_pizza_ingredients_choicebox.getSelectionModel().getSelectedItem();
+		 String amount = add_pizza_ingredient_amount_textfield.getText();
+		 String units = add_pizza_ingredient_amount_units_choicebox.getSelectionModel().getSelectedItem();
 		 //check if ingredient choosen correctly
 		 //check if amount is correct
 		 //check if units choosen correctly
-		 //show ingredient in textfield
-		 //
+		 if(indexIngredient > 0 && !amount.trim().isEmpty() && indexIUnit > 0) {
+			 strIngredient = add_pizza_ingredients_choicebox.getSelectionModel().getSelectedItem() + " " + amount + units;
+			 ingredientsText.add(strIngredient);
+			 String temp = "";
+			 for(int i = 0; i < ingredientsText.size(); i++) {
+				 if(i < ingredientsText.size()-1) {
+					 temp = temp + ingredientsText.get(i) + ", ";
+				 }else {
+					 temp = temp + ingredientsText.get(i) + ". "; 
+				 } 
+			 }
+			//show ingredient in textfield
+			 add_pizza_ingredients_textarea.setText("");
+			 add_pizza_ingredients_textarea.setText(temp);
+			 
+			 if(units.equals("g")) {
+				 ingredients.put(ingredient, new Measurement(Double.valueOf(amount), Unit.g));
+			 }else if(units.equals("kg")) {
+				 ingredients.put(ingredient, new Measurement(Double.valueOf(amount), Unit.kg));
+			 }
+		} 
+	 }
+	 
+	 @FXML
+	 private void clearIngredient(ActionEvent event) {
+		 add_pizza_ingredient_amount_units_choicebox.getSelectionModel().selectFirst();
+		 add_pizza_ingredients_choicebox.getSelectionModel().selectFirst();
+		 ingredientsText.clear();
+		 ingredients.clear();
+		 add_pizza_ingredients_textarea.setText("");
+		 add_pizza_ingredient_amount_textfield.setText("");
 	 }
 	
 	/**
@@ -68,8 +108,8 @@ public class ManagerAddPizzaController {
 		String name = pizza_name_textfield.getText();
 		String descriptionSv = pizza_description_sv_textfield.getText();
 		String descriptionEn = pizza_description_en_textfield.getText();
-		int price30cm = Integer.valueOf(pizza_price_textfield.getText());
-		Pizza pizza = new Pizza(name, 0, price30cm, 0, descriptionSv, descriptionEn);	
+		int price30cm = Integer.valueOf(pizza_price_textfield.getText());		
+		Pizza pizza = new Pizza(name, 0, price30cm, 0, 0, descriptionSv, descriptionEn, ingredients);	
 		
 		//Add new pizza to database addPizza(pizza);
 		try {
