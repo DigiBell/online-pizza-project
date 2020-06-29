a2bf2d72a0fe026ff4f679f9abbe6a25cd461514
André Hansson <andre.hansson92@gmail.com>
2020-06-30 01:01:17 +0200

    Employee orders

--- src/main/java/com/mycompany/onlinepizzaproject/EmployeeOrderTabController.java
+++ src/main/java/com/mycompany/onlinepizzaproject/EmployeeOrderTabController.java
@@ -7,8 +7,10 @@ import java.util.Date;
 import java.util.List;
 
 import com.mycompany.onlinepizzaproject.backend.Order;
+import com.mycompany.onlinepizzaproject.backend.Order.Status;
 import com.mycompany.onlinepizzaproject.backend.API;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +18,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -26,22 +30,28 @@ public class EmployeeOrderTabController {
 	@FXML private MainController mainController;
     @FXML private TableView<Order> ordersTable;
     @FXML private TableColumn<Order, String> orders_order_id_column;
-    @FXML private TableColumn<Order, String> orders_user_id_column;
     @FXML private TableColumn<Order, String> orders_date_column;
-    @FXML private TableColumn<Order, String[]> orders_products_column;
+    @FXML private TableColumn<Order, String> orders_user_email_column;
     @FXML private TableColumn<Order, String> orders_total_price_column;
+    @FXML private TableColumn<Order, String> orders_status_column;
     @FXML private DatePicker orders_from_date_picker;
     @FXML private DatePicker orders_to_date_picker;
+    @FXML private ChoiceBox<Object> orders_category_choicebox = new ChoiceBox<Object>();
 
 
     @FXML
     private void initialize(){
         mainController = MainController.getMainControllerInstance();
+        
+        orders_category_choicebox.setItems(FXCollections.observableArrayList("All", new Separator(), "Placed", "In progress"));	
+        orders_category_choicebox.getSelectionModel().selectFirst();
+
         orders_order_id_column.setCellValueFactory(new PropertyValueFactory<>("Id"));
-        orders_user_id_column.setCellValueFactory(new PropertyValueFactory<>("Customer"));
         orders_date_column.setCellValueFactory(new PropertyValueFactory<>("Date"));
-        orders_products_column.setCellValueFactory(new PropertyValueFactory<>("OrderLines"));
+        orders_user_email_column.setCellValueFactory(new PropertyValueFactory<>("Customer"));
         orders_total_price_column.setCellValueFactory(new PropertyValueFactory<>("TotalPrice"));
+        orders_status_column.setCellValueFactory(new PropertyValueFactory<>("Status"));
+        
     }
 
     /**
@@ -49,11 +59,59 @@ public class EmployeeOrderTabController {
      * @param event
      */
     @FXML
-    private void showAll(ActionEvent event){ // NEED A CHANGE, GET ORDERS WITH STATUS PLACED FROM DATABASE, OR SORT ON CLIENT SIDE.
-    	LocalDate dateFrom = orders_from_date_picker.getValue();
-        LocalDate dateTo = orders_to_date_picker.getValue();
-        ordersTable.getItems().clear();        
-        ordersTable.getItems().addAll(API.getOpenOrders());
+    private void showAll(ActionEvent event) {
+        LocalDate localDateFrom = orders_from_date_picker.getValue();
+        LocalDate localDateTo = orders_to_date_picker.getValue();
+        
+        ordersTable.getItems().clear();
+        
+        String category = (String) orders_category_choicebox.getSelectionModel().getSelectedItem();
+        
+        Date from = null;
+        Date to = null;
+        
+ 		if(localDateFrom != null) {
+ 			
+ 			from = Date.from(orders_from_date_picker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
+ 			
+ 			if(localDateTo != null) {
+ 				to = Date.from(orders_to_date_picker.getValue().atStartOfDay(ZoneId.systemDefault()).toInstant());
+ 			} else {
+ 				to = new Date();
+ 				orders_to_date_picker.setValue(to.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+ 			}         			
+ 		}
+        
+		switch (category) {
+		case "All":
+			if (from != null && to != null) {
+				ordersTable.getItems().addAll(API.getOpenOrders(from, to));
+			} else {
+				ordersTable.getItems().addAll(API.getOpenOrders());
+			}		
+			break;
+		case "Placed":
+			if (from != null && to != null) {
+				ordersTable.getItems().addAll(API.getOrders(Status.placed, from, to));
+			} else {
+				ordersTable.getItems().addAll(API.getOrders(Status.placed));
+			}		
+			break;
+		case "In progress":
+			if (from != null && to != null) {
+				ordersTable.getItems().addAll(API.getOrders(Status.inProgress, from, to));
+			} else {
+				ordersTable.getItems().addAll(API.getOrders(Status.inProgress));
+			}		
+			break;
+		}       
+        
+    }
+    
+    @FXML
+    private void clearDates(ActionEvent event) {
+    	orders_from_date_picker.setValue(null);
+    	orders_to_date_picker.setValue(null);
     }
     
     /**
@@ -69,7 +127,7 @@ public class EmployeeOrderTabController {
             Alert alert = new Alert(Alert.AlertType.WARNING, "Choose an order from table.", ButtonType.OK);
             alert.showAndWait();
         }else{
-            //mainController.setIngredientToChange(order);
+        	mainController.setOrderToChange(order);
         	showOrderDetailsView();
         }
     }
