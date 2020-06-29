0814536ccd34fc0a2e6d52b4e79072c7ed278548
Anna_Ahlquist <DigiBell@users.noreply.github.com>
2020-06-30 00:00:10 +0200

    added choicebox with countries to create employee

--- src/main/java/com/mycompany/onlinepizzaproject/CreateEmployeeAccountController.java
+++ src/main/java/com/mycompany/onlinepizzaproject/CreateEmployeeAccountController.java
@@ -1,13 +1,23 @@
 package com.mycompany.onlinepizzaproject;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mycompany.onlinepizzaproject.Model.Account;
 import com.mycompany.onlinepizzaproject.backend.API;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -22,6 +32,7 @@ public class CreateEmployeeAccountController {
     @FXML private TextField create_street_field;
     @FXML private TextField create_postal_code_field;
     @FXML private TextField create_phone_number_field;
+    @FXML private ChoiceBox<String> create_country_choicebox = new ChoiceBox<String>();
 
     private Alert alert;
     private Stage stage;
@@ -30,6 +41,26 @@ public class CreateEmployeeAccountController {
     @FXML
     private void initialize(){
         mainController = MainController.getMainControllerInstance();
+        
+        File file = new File("C:\\Users\\akira\\eclipse-workspace\\online-pizza-project\\text\\countries.txt");
+        BufferedReader br;
+        List<String> countries = new ArrayList<>();
+        countries.add("Select country");
+		try {
+			br = new BufferedReader(new FileReader(file));
+			String str;
+	        while ((str = br.readLine()) != null){
+	        	System.out.println(str);
+	        	countries.add(str);
+	        }
+	        br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+        create_country_choicebox.setItems(FXCollections.observableArrayList(countries));
+        create_country_choicebox.getSelectionModel().selectFirst();
     }
 
     @FXML
@@ -103,8 +134,8 @@ public class CreateEmployeeAccountController {
         }else{ newAccount.setFirstName(create_first_name_field.getText()); }
         if (create_last_name_field.getText().isEmpty()){ return false;
         }else{ newAccount.setLastName(create_last_name_field.getText()); }
-        if (create_country_field.getText().isEmpty()){ return false;
-        }else{ newAccount.setCountry(create_country_field.getText()); }
+        if (create_country_choicebox.getSelectionModel().getSelectedIndex() == 0){ return false;
+        }else{ newAccount.setCountry(create_country_choicebox.getSelectionModel().getSelectedItem()); }
         if (create_city_field.getText().isEmpty()){ return false;
         }else{ newAccount.setCity(create_city_field.getText()); }
         if (create_street_field.getText().isEmpty()){ return false;
